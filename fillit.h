86fd22013316096eb15b2ab223e642d899efc5fa
Mariya RAKHMAN <mrakhman@student.42.fr>
2018-05-18 16:53:50 +0200

    Masha: made error functions static and removed from fillit.h

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: mrakhman <mrakhman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/10 16:10:25 by rfumeron          #+#    #+#             */
-/*   Updated: 2018/05/18 16:45:12 by mrakhman         ###   ########.fr       */
+/*   Updated: 2018/05/18 16:53:17 by mrakhman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 # include <sys/stat.h>
 # include "libft/libft.h"
+
 // # define FILE_ERROR (-1)
 // # define MALLOC_ERROR (-2)
 // # define CAT_ERROR (-3)
@@ -31,19 +32,19 @@ typedef	struct	s_point
 typedef struct	s_figure
 {
 	t_point	points[4];
-	int	xmax;
-	int	ymax;
-	char letter;
+	int		xmax;
+	int		ymax;
+	char	letter;
 }				t_figure;
 
-char		*ret_file_string(char *filename);
-char		**initialize_field(int sqr_len);
-int			smallest_square_side(int tetrimino_count);
-int			error_check(char *str);
-int			count_shapes(char *str);
-void		correct_points(t_figure *pfigure);
-t_figure	*store_shapes(char *str);
-t_figure	initialize_figure(char *str, int index);
-t_point		initialize_point(char *str, int index);
+char			*ret_file_string(char *filename);
+char			**initialize_field(int sqr_len);
+int				smallest_square_side(int tetrimino_count);
+int				error_check(char *str);
+int				count_shapes(char *str);
+void			correct_points(t_figure *pfigure);
+t_figure		*store_shapes(char *str);
+t_figure		initialize_figure(char *str, int index);
+t_point			initialize_point(char *str, int index);
 
 #endif
